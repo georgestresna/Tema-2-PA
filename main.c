3b83b9908a967cd012e3f979732c6c5e95216c6d
George Stresna <68332128+georgestresna@users.noreply.github.com>
2024-05-22 00:36:51 +0300

    ex2, 100/100, eliberare memorie, comentarii cod

--- main.c
+++ main.c
@@ -6,11 +6,10 @@
 
 
 float calculatorScor(int r){
-    int l=5; 
+    int l=6; 
     float q=0.15;
-
     //formula
-    float scor = (q * pow((2-q),r))/(pow(2,l) + pow((2-q),l)*(q-1));
+    float scor = (q * pow((2-q),r))/(pow(2,l) + (pow((2-q),l)*(q-1)));
     return scor;
 }
 
@@ -68,6 +67,46 @@ void scoreboard(Node**head, Graph**g){
     }
 }
 
+void listCopy(Node*head, Node**headRank){
+    while(head){
+        addAtEnd(headRank, head->echipa);
+        head=head->next;
+    }
+}
+
+void listEvaluate(Node**headRank, Graph*g){
+    Node*headCopy=*headRank;
+    for(int i=0; i<32; i++){
+        int count=0;        //nr de victorii
+        for(int j=0; j<32; j++){
+            if(g->a[j][i] == 1) count++;
+        }
+        headCopy->echipa.teamScore=count;
+        headCopy=headCopy->next;
+    }
+}
+
+void listOutput(Node*headRank, FILE*scor){
+    for(int i=0; i<6; i++){     //caut echipele cu i winuri
+        Node*headCopy=headRank;
+        float scorCurent=calculatorScor(i);     //scor pt i winuri
+        while(headCopy){
+            if((int)headCopy->echipa.teamScore == i)    //daca o echipa are i winuri, atunci
+                fprintf(scor, "%.4f%s\n", scorCurent, headCopy->echipa.name);
+            headCopy=headCopy->next;
+        }
+    }
+}
+
+void freeList(Node* head){
+   Node* tmp;
+   while (head != NULL){
+       tmp = head;
+       head = head->next;
+       free(tmp);
+    }
+}
+
 int main(int argc, char *argv[]){
 
 	FILE * in = fopen(argv[1], "r");
@@ -76,11 +115,28 @@ int main(int argc, char *argv[]){
 
     Node *head=NULL;
     readFile(&head, in);     //voi stoca in float scorurile initiale, din fisier
+    Node *headRank=NULL; 
+    listCopy(head, &headRank);  //copie la lista pt punctul 2
 
-    //ptc1
+    //pct1
     Graph *g=create();      //creaza graf
-    scoreboard(&head, &g);  //populate mtrice
+    scoreboard(&head, &g);  //populare matrice
     printGraph(g, graf);    //print matrice
+
+    float headScore=calculatorScor(0);  //pana la 5 i, inclusiv
+
+    //pct2, lucrez cu headRank
+    listEvaluate(&headRank, g);     //am schimbat float-urile cu pct fin
+    listOutput(headRank, scor);
+    //while(headRank){printf("%.4f %s\n", headRank->echipa.teamScore, headRank->echipa.name); headRank=headRank->next;}
+
+    //eliberare memorie
+    free(head);             //prima lista, ramane doar castigatorul
+    freeList(headRank);     //a doua lista, intreaga
+    for(int i = 0; i < 32; i++)      //matricea de adiacenta a grafului
+        free(g->a[i]);
+    free(g->a);
+    free(g);                //graful
 
     fclose(in);
     fclose(graf);
